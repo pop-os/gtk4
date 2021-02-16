ea40c2b85fd96051cfb15d5cfe7e0d194793551e
Simon McVittie <smcv@debian.org>
2021-02-16 00:06:26 +0000

    d/log-reftests.py: Collect more test results

--- debian/log-reftests.py
+++ debian/log-reftests.py
@@ -7,22 +7,55 @@ import sys
 from pathlib import Path
 
 if __name__ == '__main__':
+    for node in Path('testsuite', 'gsk', 'compare').glob('*.node'):
+        for outputs in (
+            Path(
+                'debian', 'build', 'deb', 'testsuite', 'gsk', 'compare',
+                'opengl', 'x11',
+            ),
+            Path(
+                'debian', 'build', 'deb', 'testsuite', 'gsk', 'compare',
+                'broadway', 'x11',
+            ),
+        ):
+            diff = (outputs / (node.stem + '.diff.png'))
+
+            if diff.exists():
+                ref = Path('testsuite', 'gsk', 'compare', node.stem + '.png')
+                out = (outputs / (node.stem + '.out.png'))
+
+                for path in (ref, out, diff):
+                    if path.exists():
+                        print('begin-base64 644 %s' % path)
+                        sys.stdout.flush()
+                        with open(path, 'rb') as reader:
+                            base64.encode(reader, sys.stdout.buffer)
+                        print('====')
+                        print('')
+
+                print('')
+
     for ui in Path('testsuite', 'reftests').glob('*.ui'):
         for outputs in (
-            Path('debian', 'build', 'deb', 'testsuite', 'reftests', 'output', 'x11'),
+            Path(
+                'debian', 'build', 'deb', 'testsuite', 'reftests',
+                'output', 'x11',
+            ),
         ):
             diff = (outputs / (ui.stem + '.diff.png'))
 
-        if diff.exists():
-            ref = (outputs / (ui.stem + '.ref.png'))
-            out = (outputs / (ui.stem + '.out.png'))
+            if diff.exists():
+                ref = (outputs / (ui.stem + '.ref.png'))
+                out = (outputs / (ui.stem + '.out.png'))
 
-            for path in (ref, out, diff):
-                print('begin-base64 644 %s' % path)
-                sys.stdout.flush()
-                with open(path, 'rb') as reader:
-                    base64.encode(reader, sys.stdout.buffer)
-                print('====')
+                for path in (ref, out, diff):
+                    if path.exists():
+                        print('')
+                        print('begin-base64 644 %s' % path)
+                        sys.stdout.flush()
+                        with open(path, 'rb') as reader:
+                            base64.encode(reader, sys.stdout.buffer)
+                        print('====')
+                        print('')
+
                 print('')
-
-            print('')
